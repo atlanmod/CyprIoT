07fa748ca0d6d2b4950e5bbdc8538d79a52a9fe0
ilberium <wvi@pm.me>
2019-06-21 00:00:41 -0400

    diverse minor fixes

--- org.atlanmod.cypriot.generator/src/main/java/org/atlanmod/cypriot/generator/main/App.java
+++ org.atlanmod.cypriot.generator/src/main/java/org/atlanmod/cypriot/generator/main/App.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.atlanmod.cypriot.CypriotStandaloneSetup;
 import org.atlanmod.cypriot.cyprIoT.CyprIoTModel;
 import org.atlanmod.cypriot.cyutil.Helpers;
 import org.atlanmod.cypriot.generator.network.NetworkGenerator;
@@ -11,6 +12,15 @@ import org.atlanmod.cypriot.generator.plugins.PluginLoader;
 import org.atlanmod.cypriot.generator.transform.BindingTransformation;
 import org.atlanmod.cypriot.generator.transform.PolicyEnforcementTransformation;
 import org.atlanmod.cypriot.generator.utilities.NetworkHelper;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.m2m.atl.emftvm.EmftvmFactory;
+import org.eclipse.m2m.atl.emftvm.ExecEnv;
+import org.eclipse.m2m.atl.emftvm.Metamodel;
+import org.eclipse.m2m.atl.emftvm.Model;
+import org.thingml.xtext.ThingMLStandaloneSetup;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -21,21 +31,22 @@ public class App implements Runnable {
 
 	static final Logger log = LogManager.getLogger(App.class.getName());
 
-	@Option(names = { "-i", "--input" }, required = true, paramLabel = "INPUT", description = "The input file for the code generator")
+	@Option(names = { "-i", "--input" }, required = false, paramLabel = "INPUT", description = "The input file for the code generator")
 	File cypriotInputFile;
 
-	@Option(names = { "-o", "--output" }, required = true, paramLabel = "OUTPUT", description = "The output directory")
+	@Option(names = { "-o", "--output" }, required = false, paramLabel = "OUTPUT", description = "The output directory")
 	File cypriotOutputDirectory;
 
-	@Option(names = { "-c", "--config" }, required = true, paramLabel = "CONFIG", description = "The configuration file")
+	@Option(names = { "-c", "--config" }, required = false, paramLabel = "CONFIG", description = "The configuration file")
 	File cypriotConfigFile;
 	
 	@Option(names = { "-d", "--disable-plugins" }, description = "The configuration file")
     boolean disablePlugin;
 
 	public void run() {
+			
 		NetworkHelper.showProjectVersioInConsole();
-		
+		/*
 		// Network Model Loading
 		CyprIoTModel model = Helpers.loadModelFromFile(cypriotInputFile, CyprIoTModel.class);
 		
@@ -53,7 +64,7 @@ public class App implements Runnable {
 		networkGenerator.setBinding(new BindingTransformation());
 		networkGenerator.setEnfocePolicy(new PolicyEnforcementTransformation());
 		networkGenerator.generate();
-
+		*/
 	}
 
 	public static void main(String[] args) {
