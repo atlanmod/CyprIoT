ec9a14f93e25a50410505b3fb7ca33d988a502a8
ilberium <wvi@pm.me>
2019-07-08 16:55:22 +0200

    add automate cypriot deployement

---
+++ .travis/deploy-update-site.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DEPLOY_REPO='https://github.com/atlanmod/updates'
+export TARGET_BRANCH="master"
+export KEEP_HISTORY=false
+export SRC_FOLDER="org.atlanmod.cypriot.update/target/repository"
+export DEST_FOLDER="cypriot/snapshot"
+export DEPLOY_KEY="deploy-key-updates"
+
+export OUT_DIR="out-updates"
+
+# Add an index.html that instructs users browing the update site URL in a
+# browser to use it in Eclipse instead.
+#
+# We put the contents of the `static` folder into the SRC_FOLDER because the
+# DEST_FOLDER will be erased and populated by the safe-deploy script.
+cp -r update/static/. ${SRC_FOLDER}
+
+bash .travis/safe-deploy.sh
---
+++ .travis/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e # Exit with nonzero exit code if anything fails
+
+# These add nice folds in the Travis log
+fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
+
+export ENCRYPTION_LABEL="b7b263ca9d8b"
+export COMMIT_AUTHOR_NAME="Travis CI"
+export COMMIT_AUTHOR_EMAIL="deploy@travis.org"
+
+fold_start deploy.updates "Deploy update site"
+bash .travis/deploy-update-site.sh
+fold_end deploy.updates
