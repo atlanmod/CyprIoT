4d38465589f44060e24b416caf505b068f384f49
ilberium <wvi@pm.me>
2019-09-18 11:37:22 +0200

    add nodered plugin

---
+++ generator/src/main/java/org/atlanmod/cypriot/generator/plugins/installed/NodeRedPlugin.java
@@ -0,0 +1,49 @@
+package org.atlanmod.cypriot.generator.plugins.installed;
+
+import java.io.File;
+
+import org.apache.logging.log4j.Logger;
+import org.atlanmod.cypriot.cyprIoT.CyprIoTModel;
+import org.atlanmod.cypriot.generator.plugins.Plugin;
+import org.atlanmod.cypriot.generator.utilities.AcceleoStandaloneCompiler;
+
+public class NodeRedPlugin implements Plugin {
+
+	@Override
+	public String getID() {
+		return "nodeRedGen";
+	}
+
+	@Override
+	public void attach(Logger log) {
+		log.info("Loading "+getID()+" plugin...");
+	}
+
+	@Override
+	public CyprIoTModel initiliaze() {
+		return null;
+	}
+
+	@Override
+	public CyprIoTModel transform(CyprIoTModel model) {
+		return null;
+	}
+
+	@Override
+	public void generate(CyprIoTModel model, File outputDirectory) {
+		AcceleoStandaloneCompiler acceleoStandaloneCompiler = new AcceleoStandaloneCompiler();
+		AcceleoStandaloneCompiler.MODULE_FILE_NAME="/org/atlanmod/cypriot/generator/templates/nodered";
+		acceleoStandaloneCompiler.generateAcceleo(model,outputDirectory+File.separator+"external-gen");	
+	}
+
+	@Override
+	public void verify(CyprIoTModel model) {
+
+	}
+
+	@Override
+	public void deploy(CyprIoTModel model) {
+
+	}
+
+}
