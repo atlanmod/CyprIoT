3f5d9cbe63395aead8559f3c4f939cdfcd5cd536
ilberium <wvi@pm.me>
2019-09-22 01:14:21 +0200

    minor fix

---
+++ language/org.atlanmod.cypriot.ui/src/org/atlanmod/cypriot/ui/CyprIoTLanguageColoring.java
@@ -0,0 +1,6 @@
+package org.atlanmod.cypriot.ui;
+
+import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+
+public class CyprIoTLanguageColoring extends DefaultHighlightingConfiguration{
+}
